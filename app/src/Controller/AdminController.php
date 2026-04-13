5ccca90f6d9726907903fc3ca8031b3a70bdb4c2
MohandBir <birmoho@gmail.com>
2026-04-13 15:25:07 +0200

    ticket10: Implémenter la modification d’une phrase

--- app/src/Controller/AdminController.php
+++ app/src/Controller/AdminController.php
@@ -23,26 +23,30 @@ final class AdminController extends AbstractController
         if ( !$this->IsGranted('ROLE_ADMIN')) {
             return $this->redirectToRoute('app_home');
         }
-        return $this->render('admin/index.html.twig', [
+        return $this->render('admin/sentence/index.html.twig', [
             'sentences' => $sentenceRepo->findBy(
                 [],
                 ['createdAt' => 'DESC'])
         ]);
     }
 
-    #[Route('/admin/add', name: 'app_admin_add')]
+    #[Route('/admin/save/{id?null}', name: 'app_admin_save')]
     // #[IsGranted('ROLE_ADMIN')]
-    public function add(Request $request, EntityManagerInterface $em): Response
+    public function save(?Sentence $sentence, Request $request, EntityManagerInterface $em): Response
     {
         if ( !$this->IsGranted('ROLE_ADMIN')) {
             return $this->redirectToRoute('app_home');
         }
+
+        $isUpdated = (bool)$sentence;
+
         // creation de formulaire add
-        $form = $this->createForm(SentenceType::class);
+        $form = $this->createForm(SentenceType::class, $sentence);
         $form->handleRequest($request);
         $data = $form->getData();
+
+        // gestion de soumission du formulaire
         if ($form->isSubmitted() && $form->isValid()) {
-            $sentence = new Sentence;
             $sentence->setContent($data->getContent())
             ->setCreatedAt(new DateTimeImmutable())
             ->setLikes(0)
@@ -51,13 +55,15 @@ final class AdminController extends AbstractController
             $em->persist($sentence);
             $em->flush();
 
-            $this->addFlash('success', 'La phrase est ajoutée avec succès !');
+            $action = ($isUpdated) ? substr($sentence->getContent(), 0, 20) . '..." est modifié' : substr($sentence->getContent(), 0, 10) . '...\"] ajoutée' ;
+            $this->addFlash('success', "La phrase \"$action avec succès !");
             
             return $this->redirectToRoute('app_admin_index');
         }
 
-        return $this->render('admin/add.html.twig', [
-            'formView' => $form->createView()
+        return $this->render('admin/sentence/save.html.twig', [
+            'formView' => $form->createView(),
+            'isUpdated' => $isUpdated,
         ]);
     }
 }
