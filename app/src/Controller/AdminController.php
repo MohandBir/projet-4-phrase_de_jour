8882ebf0c2c5f5e83cf5ee70d0e93d86cddc1dd5
MohandBir <birmoho@gmail.com>
2026-04-13 13:03:22 +0200

    ticket9: Implémenter l’ajout d’une phrase

--- app/src/Controller/AdminController.php
+++ app/src/Controller/AdminController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Sentence;
 use App\Entity\User;
+use App\Form\SentenceType;
 use App\Repository\SentenceRepository;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -19,7 +24,40 @@ final class AdminController extends AbstractController
             return $this->redirectToRoute('app_home');
         }
         return $this->render('admin/index.html.twig', [
-            'sentences' => $sentenceRepo->findAll()
+            'sentences' => $sentenceRepo->findBy(
+                [],
+                ['createdAt' => 'DESC'])
+        ]);
+    }
+
+    #[Route('/admin/add', name: 'app_admin_add')]
+    // #[IsGranted('ROLE_ADMIN')]
+    public function add(Request $request, EntityManagerInterface $em): Response
+    {
+        if ( !$this->IsGranted('ROLE_ADMIN')) {
+            return $this->redirectToRoute('app_home');
+        }
+        // creation de formulaire add
+        $form = $this->createForm(SentenceType::class);
+        $form->handleRequest($request);
+        $data = $form->getData();
+        if ($form->isSubmitted() && $form->isValid()) {
+            $sentence = new Sentence;
+            $sentence->setContent($data->getContent())
+            ->setCreatedAt(new DateTimeImmutable())
+            ->setLikes(0)
+            ->setCategory($data->getCategory())
+            ;
+            $em->persist($sentence);
+            $em->flush();
+
+            $this->addFlash('success', 'La phrase est ajoutée avec succès !');
+            
+            return $this->redirectToRoute('app_admin_index');
+        }
+
+        return $this->render('admin/add.html.twig', [
+            'formView' => $form->createView()
         ]);
     }
 }
